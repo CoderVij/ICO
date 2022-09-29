bfdf56916c63d71683dd9cec119e2ed759a99bac
CoderVij <contactfreakout@gmail.com>
2022-09-29 12:48:20 +0530

    created next app

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-const { ether} = require("hardhat");
+const { ethers} = require("hardhat");
 require("dotenv").config({path:".env"});
 const {CRYPTO_DEVS_NFT_CONTRACT_ADDRESS} = require("../constants");
 
@@ -7,7 +7,7 @@ async function main()
 {
   const cryptoDevsNFTContract = CRYPTO_DEVS_NFT_CONTRACT_ADDRESS;
 
-  const cryptoDevsTokenContract = await ether.getContractFactory("CryptoDevToken");
+  const cryptoDevsTokenContract = await ethers.getContractFactory("CryptoDevToken");
 
   const deployedCryptoDevsTokenContract = await cryptoDevsTokenContract.deploy(cryptoDevsNFTContract);
 
